e0bbd82a9291e07dcc55d204fdfe1046028886e6
ahmedyasin21 <coolahmed21@gmail.com>
2021-11-01 12:43:02 +0500

    settings updated

--- source/settings.py
+++ source/settings.py
@@ -96,8 +96,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 MAILER_EMAIL_BACKEND = EMAIL_BACKEND  
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'Dummyfirst54@gmail.com' 
-EMAIL_HOST_PASSWORD = 'Allahis_1'  
+EMAIL_HOST_USER = 'XXXXXXX@gmail.com' 
+EMAIL_HOST_PASSWORD = 'XXXXXXX  
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
